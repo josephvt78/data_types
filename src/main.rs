5063d08af0a9707e00a1b7503fb0421404b3f40b
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-11-29 19:47:29 -0500

    arithmetic operations and data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     let _guess: isize = "-42".parse().expect("Not a number!");
+
+    let _x = 2.0; // f64
+
+    let _y: f32 = -3.0;
+
+    //addition
+    let sum = 5 + 10;
+    println!("Sum: {sum}");
+
+    //subtraction
+    let differnce = 95.5 - 4.3;
+    println!("Difference {differnce}");
+
+    //division
+    let quotient = 56.7 / 32.2;
+    println!("Quotient {quotient}");
+    let truncated = -5 /3;
+    println!("Quotient {truncated}");
+    let non_truncated: f32 = -5 as f32 /3 as f32;
+    println!("Quotient {non_truncated}");
+
+    //remainder
+    let remainder = 43 % 5;
+    println!("Remainder {remainder}");
+
 }
